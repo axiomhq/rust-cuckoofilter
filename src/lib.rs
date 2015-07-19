ff446c0b6731c6675b4c8610157c4607b047b668
Matthijs Hofstra <thiezz@gmail.com>
2015-07-19 22:21:23 +0200

    Perform fewer allocations, use std::hash::Hasher infrastructure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Cuckoo filter probabilistic data structure for membership testing and cardinlaity counting.
+//! Cuckoo filter probabilistic data structure for membership testing and cardinality counting.
 //!
 //! # Usage
 //!
@@ -23,31 +23,39 @@ mod bucket;
 mod util;
 
 extern crate rand;
-extern crate farmhash;
 extern crate byteorder;
 
-use farmhash::{FarmHasher};
 use bucket::{Bucket, Fingerprint, BUCKET_SIZE};
 use util::{get_fai, get_alt_index, FaI};
 use rand::{Rng};
 use std::iter::{repeat};
-use std::hash::{Hasher, Hash};
+use std::hash::{SipHasher, Hasher, Hash};
 use std::marker::{PhantomData};
 
-pub const MAX_REBUCKET: usize = 500;
+/// If insertion fails, we will retry this many times.
+pub const MAX_REBUCKET: u32 = 500;
+
+/// The default number of buckets.
+pub const DEFAULT_CAPACITY: u64 = 1 << 20 - 1;
 
 // A cuckoo filter class exposes a Bloomier filter interface,
 // providing methods of add, delete, contains.
-pub struct CuckooFilter<H = FarmHasher> {
+pub struct CuckooFilter<H> {
     buckets: Box<[Bucket]>,
     len: u64,
     _hasher: std::marker::PhantomData<H>,
 }
 
-impl CuckooFilter<FarmHasher> {
-  /// Constructs a Cockoo Filter with default capacity and hasher.
-  pub fn new() -> CuckooFilter<FarmHasher> {
-    Self::with_capacity(1000000)
+impl Default for CuckooFilter<SipHasher> {
+  fn default() -> Self {
+    CuckooFilter::new()
+  }
+}
+
+impl CuckooFilter<SipHasher> {
+  /// Construct a CuckooFilter with default capacity and hasher.
+  pub fn new() -> CuckooFilter<SipHasher> {
+    Self::with_capacity(DEFAULT_CAPACITY)
   }
 }
 
@@ -71,31 +79,44 @@ impl<H> CuckooFilter<H>
         }
     }
 
-    /// Returns a Cuckoo Filter with a default max Capacity 1000000 items/
-    pub fn default() -> CuckooFilter<H> {
-        CuckooFilter::with_capacity(1000000)
-    }
-
     /// Checks if `data` is in the filter.
     pub fn contains<T: ?Sized + Hash>(&mut self, data: &T) -> bool {
         let FaI { fp, i1, i2 } = get_fai::<T, H>(data);
         let len = self.buckets.len();
-        let b1 = self.buckets[i1%len].get_fingerprint_index(fp);
-        let b2 = self.buckets[i2%len].get_fingerprint_index(fp);
-
-        b1.or(b2).is_some()
+        self.buckets[i1%len].get_fingerprint_index(fp).or(
+        self.buckets[i2%len].get_fingerprint_index(fp)).is_some()
     }
 
     /// Adds `data` to the filter. Returns true if the insertion was successful.
     pub fn add<T: ?Sized + Hash>(&mut self, data: &T) -> bool {
-        let FaI { fp, i1, i2 } = get_fai::<T, H>(data);
-        self.put(fp, i1) || self.put(fp, i2) || self.reinsert(fp, i2)
+        let fai = get_fai::<T, H>(data);
+        if self.put(fai.fp, fai.i1) || self.put(fai.fp, fai.i2) {
+          return true;
+        }
+        let len = self.buckets.len();
+        let mut rng = rand::thread_rng();
+        let mut i = fai.random_index(&mut rng);
+        let mut fp = fai.fp;
+        for _ in 0..MAX_REBUCKET {
+          let other_fp;
+          {
+            let loc = &mut self.buckets[i % len].buffer[rng.gen_range(0, BUCKET_SIZE)];
+            other_fp = *loc;
+            *loc = fp;
+            i = get_alt_index::<H>(other_fp, i);
+          }
+          if self.put(other_fp, i) {
+            return true;
+          }
+          fp = other_fp;
+        }
+        panic!("Map is full, could not insert item");
     }
 
     /// Adds `data` to the filter if it does not exist in the filter yet.
     /// Returns `true` if `data` was not yet present in the filter and added
     /// successfully.
-    pub fn test_and_add(&mut self, data: &[u8]) -> bool {
+    pub fn test_and_add<T: ?Sized + Hash>(&mut self, data: &T) -> bool {
         if self.contains(data) {
             false
         } else {
@@ -115,12 +136,12 @@ impl<H> CuckooFilter<H>
 
     /// Deletes `data` from the filter. Returns true if `data` existed in the
     /// filter before.
-    pub fn delete<T: ?Sized + Hash>(&mut self, data: &T) -> bool{
+    pub fn delete<T: ?Sized + Hash>(&mut self, data: &T) -> bool {
         let FaI { fp, i1, i2 } = get_fai::<T, H>(data);
-
         self.remove(fp, i1) || self.remove(fp, i2)
     }
 
+    /// Removes the item with the given fingerprint from the bucket indexed by i.
     fn remove(&mut self, fp: Fingerprint, i: usize) -> bool {
         let len = self.buckets.len();
         if self.buckets[i%len].delete(fp) {
@@ -139,21 +160,5 @@ impl<H> CuckooFilter<H>
         } else {
             false
         }
-    }
-
-    fn reinsert(&mut self, mut fp: Fingerprint, mut i: usize) -> bool {
-        for _ in 0..MAX_REBUCKET {
-            let j = rand::thread_rng().gen_range(0, BUCKET_SIZE);
-            let newfp = fp;
-            let len = self.buckets.len();
-            fp = self.buckets[i%len].buffer[j];
-            self.buckets[i%len].buffer[j] = newfp;
-            i = get_alt_index::<H>(&mut fp, i);
-            if self.put(fp, i) {
-                return true;
-            }
-        }
-
-        false
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,22 +1,5 @@
-use std::hash::{Hasher, Hash, hash};
-use ::byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
+use std::hash::{Hasher, Hash};
 use ::bucket::{Fingerprint};
-
-fn get_hash<T: ?Sized + Hash, H: Hasher + Default>(data: &T) -> [u8; 4] {
-  let mut result = [0; 4];
-  {
-    let mut hasher = <H as Default>::default();
-    data.hash(&mut hasher);
-    let _ = (&mut result[..]).write_u32::<BigEndian>(hasher.finish() as u32);
-  }
-  result
-}
-
-pub fn get_alt_index<H: Hasher + Default>(fp: &Fingerprint, i: usize) -> usize {
-    let hash = get_hash::<_, H>(&[fp.0]);
-    let alt_i = (&hash[..]).read_u32::<BigEndian>().unwrap() as usize;
-    (i ^ alt_i) as usize
-}
 
 pub struct FaI {
     pub fp: Fingerprint,
@@ -24,26 +7,59 @@ pub struct FaI {
     pub i2: usize
 }
 
-pub fn get_fai<T: ?Sized + Hash, H: Hasher + Default>(data: &T) -> FaI {
-    let hash = get_hash::<_, H>(data);
-    let f = Fingerprint(hash[0]);
-    let i1 = (&hash[..]).read_u32::<BigEndian>().unwrap() as usize;
-    let i2 = get_alt_index::<H>(&f, i1);
-    FaI {
-        fp: f, i1: i1, i2: i2
+pub fn get_alt_index<H: Hasher + Default>(fp: Fingerprint, index: usize) -> usize {
+  let mut hasher = <H as Default>::default();
+  fp.hash(&mut hasher);
+  hasher.finish() as usize ^ index
+}
+
+impl FaI {
+  fn from_data<T: ?Sized + Hash, H: Hasher + Default>(data: &T) -> FaI {
+    let i1;
+    let fp;
+    let mut n = 1;
+    loop {
+      let mut hasher = <H as Default>::default();
+      for _ in 0..n {
+        data.hash(&mut hasher);
+      }
+      let hash = hasher.finish() as usize;
+      if let Some(val) = Fingerprint::from_usize(hash) {
+        i1 = hash;
+        fp = val;
+        break;
+      }
+      n += 1;
     }
+    let i2 = get_alt_index::<H>(fp, i1);
+
+    FaI { fp: fp, i1: i1, i2: i2 }
+  }
+
+  pub fn random_index<R: ::rand::Rng>(&self, r: &mut R) -> usize {
+    if r.gen() {
+      self.i1
+    } else {
+      self.i2
+    }
+  }
+}
+
+pub fn get_fai<T: ?Sized + Hash, H: Hasher + Default>(data: &T) -> FaI {
+    FaI::from_data::<_, H>(data)
 }
 
 #[test]
 fn test_fp_and_index() {
-    let data = &"seif".as_bytes();
-    let fai = get_fai::<_, ::farmhash::FarmHasher>(data);
-    let fp = &fai.fp;
-    let i1 = fai.i1;
-    let i2 = fai.i2;
-    let i11 = get_alt_index::<::farmhash::FarmHasher>(fp, i2);
-    assert_eq!(i11, i1);
+  use std::hash::SipHasher;
+  let data = "seif";
+  let fai = get_fai::<_, SipHasher>(data);
+  let fp = fai.fp;
+  let i1 = fai.i1;
+  let i2 = fai.i2;
+  let i11 = get_alt_index::<SipHasher>(fp, i2);
+  assert_eq!(i11, i1);
 
-    let i22 = get_alt_index::<::farmhash::FarmHasher>(fp, i11);
-    assert_eq!(i22, i2);
+  let i22 = get_alt_index::<SipHasher>(fp, i11);
+  assert_eq!(i22, i2);
 }
