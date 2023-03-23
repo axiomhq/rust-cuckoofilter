0042143a706e8cd8151fbd6c6f59e8e0a39a6d72
BHouwens <zhion360@gmail.com>
2023-03-23 15:23:36 +0100

    Add derivations to exported filter; Clean deprecated errors

--- src/lib.rs
+++ src/lib.rs
@@ -281,7 +281,7 @@ where
 }
 
 /// A minimal representation of the CuckooFilter which can be transfered or stored, then recovered at a later stage.
-#[derive(Debug)]
+#[derive(Debug, Clone, Default)]
 #[cfg_attr(feature = "serde_support", derive(Deserialize, Serialize))]
 pub struct ExportedCuckooFilter {
     #[cfg_attr(feature = "serde_support", serde(with = "serde_bytes"))]
